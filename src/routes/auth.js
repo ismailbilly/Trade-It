edcc0f821633f5b9b21fb3db6e7946ea505a5c69
Counsellor-Olumayor <dhikrnur@gmail.com>
2023-09-24 22:31:26 +0100

    feat(createCategory, updateCategory) hi

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,5 +4,6 @@ const { createUser } = require("../controllers/auth");
 const validationMiddleware = require("../middleware/validation");
 const { create } = require("../validation/user");
 router.post("/user", validationMiddleware(create), createUser);
+const { createCategory } = require("../controllers/category");
 
 module.exports = router;
